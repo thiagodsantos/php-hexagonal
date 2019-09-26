953b7323458b14cc8b6a67f3870cc56597c618f9
Thiago Santos <thiagosantos@cacdigital.com.br>
2019-09-25 21:56:01 -0300

    feat: Adicionado container e routes

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,15 @@
 <?php
 
 use Dotenv\Dotenv;
+use Slim\Factory\AppFactory;
 
 date_default_timezone_set('America/Sao_Paulo');
 
 require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+require __DIR__ . '/config/container.php';
+require __DIR__ . '/config/routes.php';
 
 (Dotenv::create(__DIR__))->load();
